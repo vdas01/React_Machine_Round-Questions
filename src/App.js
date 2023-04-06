9afb82188d113ca04f220504a4410283a487d1cc
Vishal Das <vdas53073@gmail.com>
2023-04-06 18:35:52 +0530

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useEffect, useState} from 'react'
+import Product from './Product';
+import "./App.css"
 
-function App() {
+export default function App() {
+  const [products, setproducts] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const fetchData = async ()=>{
+    const res =  await fetch("https://dummyjson.com/products?limit=100");
+    const data = await res.json();
+    setproducts(data.products);
+  }
+
+
+
+  useEffect(() => {
+      fetchData();
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="results">
+    {   
+      products.slice((page-1)*10,((page-1)*10)+10).map((prod)=>{
+        return (
+          <Product prod={prod} key={prod.id}/>
+          )
+        })
+      }
     </div>
-  );
+      
+   <div className="pagination">
+    <span className={page > 1 ? 'pre' : "pagination_disable"} onClick={()=>setPage(page-1)}>◀</span>
+    { 
+     
+       [...Array(products.length/10)].map((_,ind)=>{
+        return (
+          <>
+              <span id="curr_page" className={page === ind+1 ? "page_selected":""} key={ind} 
+              onClick={() => setPage(ind + 1)}>{ind+1}</span>
+          </>
+        )
+     })  
+    }
+     
+    <span className={page < products.length/10 ? 'nex' : "pagination_disable"}  onClick={()=>setPage(page+1)}>▶</span>
+   </div>
+      
+    </>
+  )
 }
-
-export default App;
